c7a2816f9cb36832feb77f440b88295c55135e4f
Luke Egan <lukeegan2003@gmail.com>
2025-02-12 23:19:19 +1030

    set up done. I FINALLY HAVE A WINDOW

---
+++ build/CMakeFiles/Particle_Sim.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Particle_Sim.dir/src/glad.c.o"
+  "CMakeFiles/Particle_Sim.dir/src/glad.c.o.d"
+  "CMakeFiles/Particle_Sim.dir/src/main.c.o"
+  "CMakeFiles/Particle_Sim.dir/src/main.c.o.d"
+  "Particle_Sim"
+  "Particle_Sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Particle_Sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
