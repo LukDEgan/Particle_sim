a9b5ac571fc909f3164b8c9d6a485b6ba20fb3ef
Luke Egan <lukeegan2003@gmail.com>
2025-02-19 17:07:16 +1030

    made a spinning cube

--- shaders/base_vertex.glsl
+++ shaders/base_vertex.glsl
@@ -3,8 +3,11 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 ourColor;
 out vec3 color;
 uniform mat4 transform;
+uniform mat4 model;
+uniform mat4 view; 
+uniform mat4 projection;
 void main()
 {
-gl_Position = transform * vec4(aPos.x, aPos.y, aPos.z, 1.0);
+gl_Position = projection * view * model * vec4(aPos, 1.0);
 color = ourColor;
 }
